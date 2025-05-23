950fb5af917ca6ac32f4af2485b14349e6c0749a
Ahmed Maher Ali Al-Maqtari <mr.numberone312@gmail.com>
2025-05-23 16:46:42 +0300

    completeing assignement 14

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -14,34 +14,54 @@ contract VaultTest is Test {
         vault = new Vault();
 
         // TODO: Fund the user with some ether using vm.deal
-
+        vm.deal(user, 16 ether);
     }
 
     function testDeposit() public {
         // TODO: prank user and call deposit with 1 ether
-       
+        vm.startPrank(user);
+        vault.deposit{value: 1 ether}();
+        vm.stopPrank();
 
         assertEq(vault.balances(user), 1 ether);
     }
 
     function testWithdraw() public {
         // TODO: prank user, deposit 2 ether, withdraw 1 ether
-
+        vm.startPrank(user);
+        vault.deposit{value: 2 ether}();
+        vault.withdraw(1 ether);
+        vm.stopPrank();
 
         assertEq(vault.balances(user), 1 ether);
     }
 
     function test_RevertWithdrawMoreThanBalance() public {
         // TODO: prank user, deposit 1 ether
-
+        vm.startPrank(user);
+        vault.deposit{value: 1 ether}();
         vm.expectRevert();
+        vault.withdraw(2 ether);
+        vm.stopPrank();
 
         // TODO: try to withdraw 2 ether
-
     }
 
     function testGetBalance() public {
         // TODO: prank user, deposit 0.5 ether, check getBalance
         // TODO: assert returned balance is 0.5 ether
+        vm.startPrank(user);
+        vault.deposit{value: 0.5 ether}();
+        uint256 b = vault.getBalance();
+        vm.stopPrank();
+        assertEq(b, 0.5 ether);
+    }
+
+    // Added test functions
+    function test_RevertDepositforZeroDeposited() public {
+        vm.startPrank(user);
+        vm.expectRevert();
+        vault.deposit{value: 0 ether}();
+        vm.stopPrank();
     }
 }
